54187726a745be869a82e96d9228f26b2159a2c9
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-04-05 17:17:47 -0400

    retrieve weather automatically

--- source/blog/stadium/README.md
+++ source/blog/stadium/README.md
@@ -1,6 +1,23 @@
 # Stadium
 
+## Retrieve weather data
+
+Coordinates: 42°22'00.4"N 71°07'36.1"W
+
+### Manually
+
 temperature in F
 humidity in %
 wind speed in mph
 collected at https://www.wunderground.com/history/daily/us/ma/east-boston/KBOS/date/2023-5-16
+
+
+### meteostat 
+
+Environment:
+
+```
+conda create -n blog python=3.12.0
+conda activate blog
+pip install meteostat numpy pandas matplotlib
+```
---
+++ source/blog/stadium/retrieve_weather_data.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import argparse
+from meteostat import Point, Hourly
+from datetime import datetime
+import pandas as pd
+
+def retrieve_weather_data(dates, times):
+
+    location = Point(42.3584, -71.1259)
+
+    date_times = pd.to_datetime([
+        f"{date} {time}" for date, time in zip(dates, times)
+    ])
+
+    start = date_times.min().to_pydatetime()
+    end = date_times.max().to_pydatetime()
+
+    data = Hourly(location, start, end)
+    data = data.fetch()
+
+    matched = data.loc[date_times]
+    return matched
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--time-data', type=str, default='times.csv')
+    parser.add_argument('--out', type=str, default='data.csv')
+    args = parser.parse_args()
+
+    df = pd.read_csv(args.time_data)
+    dates = df['date'].tolist()
+    times = ['18:00'] * len(dates)
+    data = retrieve_weather_data(dates, times)
+
+    data['date'] = dates
+    data['times'] = times
+
+    data.to_csv(args.out, index=False)
+
+
+
+if __name__ == '__main__':
+    main()
