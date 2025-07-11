3e3688313154a8ea59335e807b615580449f546e
Lucas Amoudruz <lucas.amoudruz@wanadoo.fr>
2025-07-11 16:50:46 -0400

    blog page: reactivate math and code embedding.

--- tools/blogpage.py
+++ tools/blogpage.py
@@ -4,6 +4,9 @@ import argparse
 import markdown
 import os
 
+def md_path_to_title(md_path):
+    return "blog - " + md_path.split("/")[-1].split(".md")[0]
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('md_path', type=str, help="path to md file")
@@ -17,7 +20,11 @@ def main():
         raw_md = f.read()
     processed_md = raw_md
 
-    html_content = markdown.markdown(processed_md)
+    html_content = markdown.markdown(processed_md,
+                                     extensions=['mdx_math',
+                                                 'fenced_code',
+                                                 'codehilite'])
+
 
     html_section = f"""
 <section class="markdown-body">
@@ -25,7 +32,7 @@ def main():
 </section>
 """
 
-    title = "Blog" # TODO
+    title = md_path_to_title(md_path)
 
     with open(out_html, "w") as f:
         f.write(f"""
@@ -36,6 +43,10 @@ def main():
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>{title}</title>
   <link rel="stylesheet" href="../css/main.css" />
+  <link href="css/codehilite.css" rel="stylesheet" />
+  <script type="text/javascript" async
+      src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML">
+  </script>
 </head>
 <body>
 
